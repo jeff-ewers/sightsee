d21542bb5c40e1a0b1bdcf93b30a7d3cdda8fee2
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-05-13 11:36:36 -0500

    Add images and styling

--- src/components/trips/TripsList.jsx
+++ src/components/trips/TripsList.jsx
@@ -4,16 +4,24 @@ import './TripsList.css'
 import trashIcon from '../../assets/trash.png'
 import { deleteTrip } from "../../services/tripService"
 import { useEffect, useState } from "react"
+import sample0 from '../../assets/sample0.jpeg';
+import sample1 from '../../assets/sample1.jpeg';
+import sample2 from '../../assets/sample2.jpeg';
+import sample3 from '../../assets/sample3.jpeg';
+import sample4 from '../../assets/sample4.jpeg';
+import sample5 from '../../assets/sample5.jpeg';
+import sample6 from '../../assets/sample6.jpeg';
+import sample7 from '../../assets/sample7.jpeg';
 
 
 export const TripsList = ({currentUser, trips, setTrips, updateTrips, setUpdateTrips}) => {
 
 const [localTrips, setLocalTrips] = useState([]);
+const sampleImages = [sample0, sample1, sample2, sample3, sample4, sample5, sample6, sample7];
 
 useEffect(() => {
     setLocalTrips(trips)
 }, [trips])
-
 
 document.body.style = 'background: #004F32;';
 
@@ -31,41 +39,41 @@ const removeTrip = (event, tripId) => {
     window.location.reload();
 }
 
-return (
+const getImageByKey = (tripId) => {
+    const imagePath = sampleImages[tripId];
+    return `url(${imagePath})`;
+  };
 
-        <article className="trips-list">
-        <Link key={0} to={`/trips/new`} 
-        state={
-            {currentUser: currentUser}
-        }>
-            <section className="trip">
-                <h2>New Trip</h2>
-                <h3>Create your experience</h3>
-            </section>
-        </Link>
-            {
-                localTrips?.length ? localTrips.map(trip => {
-                    return (
-                        <Link key={trip.id} to={`/trips/${trip.id}`}
-                        // pass trip as state for useLocation()
-                        state={
-                            {trip: trip}
-                            }>
-                        <section key={trip.id} className="trip">
-                            <h2>{trip.name}</h2>
-                            <h3>{trip.description}</h3>
-                            <button className='btn-delete' onClick={(event) => removeTrip(event, trip.id)}>
-                                <img src={trashIcon} alt='Delete'/>
-                            </button>
-                        </section>
-                        </Link>
-                    )
-                }) : null
-            }
-        </article>
-
-)
+  return (
+    <article className="trips-list">
+      <Link key={0} to={`/trips/new`} state={{ currentUser: currentUser }}>
+        <section className="trip" style={{ "--random-image": getImageByKey(0) }}>
+          <h2>New Trip</h2>
+          <h3>Create an experience.&nbsp;</h3>
+        </section>
+      </Link>
+      {localTrips?.length
+        ? localTrips.map((trip) => {
+            return (
+              <Link key={trip.id} to={`/trips/${trip.id}`} state={{ trip: trip }}>
+                <section
+                  key={trip.id}
+                  className="trip"
+                  style={{ "--random-image": getImageByKey(trip.id) }}
+                >
+                  <h2>{trip.name}</h2>
+                  <h3>{trip.description}</h3>
+                  <button className="btn-delete" onClick={(event) => removeTrip(event, trip.id)}>
+                    <img src={trashIcon} alt="Delete" />
+                  </button>
+                </section>
+              </Link>
+            );
+          })
+        : null}
+    </article>
+  );
+};
 
 
 
-}
